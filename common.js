dce51f357746433cea3271f1fcad4f83cb005f02
Beko97 <93424808+Beko97@users.noreply.github.com>
2021-11-04 15:25:43 +0900

    ファーストビューとスマホメニュー調節

--- common.js
+++ common.js
@@ -9,11 +9,15 @@ $(function() {
         h=d.documentElement,t=setTimeout(function(){h.className=h.className.replace(/\bwf-loading\b/g,"")+" wf-inactive";},config.scriptTimeout),tk=d.createElement("script"),f=false,s=d.getElementsByTagName("script")[0],a;h.className+=" wf-loading";tk.src='https://use.typekit.net/'+config.kitId+'.js';tk.async=true;tk.onload=tk.onreadystatechange=function(){a=this.readyState;if(f||a&&a!="complete"&&a!="loaded")return;f=true;clearTimeout(t);try{Typekit.load(config)}catch(e){}};s.parentNode.insertBefore(tk,s)
     })(document);
 
+    var windowH = parseInt($(window).height());
+    $('.mainVisual-sp').css('height', windowH);
+    console.log('ok');
+
     //page top
     $(window).scroll(function(){
         var topH = parseInt($('.search').offset().top);
         var scroll = parseInt($(window).scrollTop());
-        var windowH = parseInt($(window).height());
+        //var windowH = parseInt($(window).height());
         if (scroll > (topH - windowH)) {
             $('.pageUp').fadeIn();
         } else if (scroll < (topH - windowH)) {
